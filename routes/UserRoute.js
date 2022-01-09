56f2717c38316a31c9d1bea751e3f7dde1915eb0
kumarkrsjs <kumarkrsjs@protonmail.com>
2022-01-09 14:24:44 -0800

    added POST and GET to add and retrieve User data

---
+++ routes/UserRoute.js
@@ -0,0 +1,69 @@
+const express = require('express');
+var router = express.Router();
+
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
+
+const mongoose= require('mongoose');
+const User = require('../models/User');
+mongoose.connect('mongodb://localhost:27017/userData')
+
+  // CREATE
+router.post('/',(req,res)=>{
+    // User.create()
+    User.create(
+        {
+            name:req.body.newData.name,
+            email:req.body.newData.email,
+            password:req.body.newData.password
+        },
+        (err,data)=>{
+            if (err){
+            res.json({success: false,message: err})
+            } else if (!data){
+            res.json({success: false,message: "Not Found"})
+            } else {
+            res.json({success: true,data: data})
+            }
+        }
+    )
+})
+  
+router.route('/:id')
+  // READ
+  .get((req,res)=>{
+    User.findById(req.params.id, (err,data)=>{
+      if(err){
+        res.json(
+          {
+            success : false,
+            message: err
+          }
+        )
+      } else if(!data){
+        res.json(
+          {
+            success: false,
+            message : "User Not Found"
+          }
+        )
+      } else {
+        res.json(
+          {
+            success: true,
+            data: data
+          }
+        )
+      }
+    })
+  })
+  // UPDATE
+  .put((req,res)=>{
+    // User.findByIdAndUpdate()
+  })
+  // DELETE
+  .delete((req,res)=>{
+    // User.findByIdAndDelete()
+  });
+
+  module.exports = router ;
--- server.js
+++ server.js
@@ -1,30 +1,14 @@
 const express= require('express');
-const mongoose= require('mongoose');
+
 const bodyParser= require('body-parser');
 const port=8000;
 const app= express();
+const userRoute = require('./routes/UserRoute')
 
 app.use(bodyParser.json());
+app.use('/users', userRoute);
 
 app.listen(port, ()=>{
 	console.log(`server is listening on port:${port}`)
 })
 
-// CREATE
-app.post('/users',(req,res)=>{
-  // User.create()
-})
-
-app.route('/users/:id')
-// READ
-.get((req,res)=>{
-  // User.findById()
-})
-// UPDATE
-.put((req,res)=>{
-  // User.findByIdAndUpdate()
-})
-// DELETE
-.delete((req,res)=>{
-  // User.findByIdAndDelete()
-})
